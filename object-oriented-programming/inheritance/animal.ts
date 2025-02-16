84dd38f2f63fc71c4737c259474296a7de3e3ad8
luis.davilaa <luisedo.da@gmail.com>
2025-02-16 15:32:06 -0600

    feat(inheritance): add folder structure

---
+++ object-oriented-programming/inheritance/animal.ts
@@ -0,0 +1,13 @@
+/*
+1. Un zoologico necesita un sistema para gestionar los animales y sus comportamientos.
+    
+    Debes modelar un conjunto de clases que representen distintos tipos de animales y sus características.
+    
+    Todos los animales tiene un nombre, especie y edad. Además, todos deber poder emitir un sonido y mostrar su información básica.
+    
+    Algunos animales, como los leones, puden rugir y tienen una melena. Los elefantes pueden hacer sonar su trompa y tienen un tamaño específico (por ejemplo, pequeño, mediano o grande).
+    
+    Crea una clase base Animal con las propiedades y métodos necesarios.
+    
+    Extender la clase Animal para crear clases específicas como Leon y Elefante que tengan comportamientos y propiedades particulares.
+*/
