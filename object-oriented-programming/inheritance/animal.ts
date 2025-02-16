49c558993ba383fdcf5d6b0444abe4d2111b8a35
luis.davilaa <luisedo.da@gmail.com>
2025-02-16 15:33:12 -0600

    chore(inheritance): add animal exercise

--- object-oriented-programming/inheritance/animal.ts
+++ object-oriented-programming/inheritance/animal.ts
@@ -11,3 +11,70 @@
     
     Extender la clase Animal para crear clases específicas como Leon y Elefante que tengan comportamientos y propiedades particulares.
 */
+
+class Animal {
+  constructor (
+    private name: string,
+    private specie: string,
+    private age: number
+  ) {}
+
+  public sound() {}
+
+  public toString() {
+    return `Name: ${this.name}
+Specie: ${this.specie}
+Age: ${this.age}`
+  }
+}
+
+class Lion extends Animal {
+  constructor(
+    name: string, 
+    specie: string, 
+    age: number
+  ) {
+    super(name, specie, age)
+  }
+  sound() {
+    return "Roaaar..."
+  }
+
+  displayInfo() {
+    const attribute = "This is the Lion 🦁"
+    return `${this.toString()} 
+${attribute}`
+  }
+}
+
+type ElephantSize = "small" | "medium" | "large"
+
+class Elephant extends Animal {
+  constructor(
+    name: string, 
+    specie: string, 
+    age: number,
+    public size: ElephantSize
+  ) {
+    super(name, specie, age)
+  }
+
+  sound() {
+    return "Elephant noise..."
+  }
+
+  displayInfo() {
+    const attribute = "This is the Elephant 🐘"
+    return `${this.toString()}
+Size: ${this.size} 
+${attribute}`
+  }
+}
+
+const lion1 = new Lion("Luis", "Lion", 2)
+console.log(lion1.displayInfo())
+console.log(lion1.sound())
+
+const elephant1 = new Elephant("Eduardo", "Elephant", 4, "large")
+console.log(elephant1.displayInfo())
+console.log(elephant1.sound())
